54259536a6b904ed8a14aa3346dd75c9c7d32957
Daniel Ortiz Roa <doro1@alu.ua.es>
2024-02-13 20:14:01 +0100

    Metodo modificado

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -9,7 +9,16 @@ namespace hada_p1
     class HadaP1{
         public static double Seconds2Minutes(double s)
         {
-            return s / 60;
+            if (s == 0)
+            {
+                return 0;
+            }
+            else
+            {
+
+
+                return s / 60;
+            }
         }
 
 
