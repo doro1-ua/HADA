f56dfbe9131ddc51cb2a8870f9093d68bee29f8d
Daniel Ortiz Roa <doro1@alu.ua.es>
2024-02-13 20:35:00 +0100

    Minutes2seconds

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -20,6 +20,10 @@ namespace hada_p1
                 return s / 60;
             }
         }
+        public static double Minutes2Seconds(double m)
+        {
+            return m / 60;
+        }
 
 
     }
