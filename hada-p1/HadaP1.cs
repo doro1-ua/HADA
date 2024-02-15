1269d788c28840b3ec352da6ad527779b5505b9d
Daniel Ortiz Roa <doro1@alu.ua.es>
2024-02-15 15:47:16 +0100

    Hecho el main

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace hada_p1
+﻿namespace hada_p1
 {
     class HadaP1{
         public static double Seconds2Minutes(double s)
@@ -22,7 +16,7 @@ namespace hada_p1
         }
         public static double Minutes2Seconds(double m)
         {
-            return m / 60;
+            return m * 60;
         }
 
 
--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using hada_p1;
 namespace hada_p1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string respuesta;
+            string seguir;
+            string unidad;
+            double pasar;
+            HadaP1 obj = new HadaP1();
+            do
+            {
+
+
+                Console.WriteLine("¿De que unidad partimos s/m?");
+                respuesta = Console.ReadLine();
+                
+                if (respuesta == "s")
+                {
+                    Console.WriteLine("Introduce los segundos que quieres convertir");
+                    unidad = Console.ReadLine();
+                    pasar = double.Parse(unidad);
+                    double v = HadaP1.Seconds2Minutes(pasar);
+                    Console.WriteLine("Hemos pasado de " + pasar + " segundos a " + v +" minutos");
+                }
+                else
+                {
+                    Console.WriteLine("Introduce los minutos que quieres convertir");
+                    unidad = Console.ReadLine();
+                    pasar = double.Parse(unidad);
+                    double w =HadaP1.Minutes2Seconds(pasar);
+                    Console.WriteLine("Hemos pasado de " + pasar + " minutos a " + w + " segundos");
+                }
+                Console.WriteLine("¿Quieres seguir? Si/No");
+                seguir = Console.ReadLine();
+
+            } while (seguir == "Si");
+
         }
+
     }
 }
