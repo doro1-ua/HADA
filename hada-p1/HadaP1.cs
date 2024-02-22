320bcbe23381f3aaf2844fa2ec75cc055f72217d
Daniel Ortiz Roa <doro1@alu.ua.es>
2024-02-22 10:04:42 +0100

    Comentario del HadaP1.cs

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -1,12 +1,15 @@
 ﻿namespace hada_p1
 {
     class HadaP1{
+        //Metodo que convierte de segundos a minutos pasandole los segundos
         public static double Seconds2Minutes(double s)
         {
+            //Setencia if que si el dato pasado es 0 devuelve 0
             if (s == 0)
             {
                 return 0;
             }
+            //Si no es 0 hace la conversion y lo devuelve
             else
             {
 
@@ -14,6 +17,7 @@
                 return s / 60;
             }
         }
+        //Metodo que convierte de minutos a segundos pasandole los segundos
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
